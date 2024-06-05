ceafea0f8b5de12b7e09142598783301de52dea2
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-06-05 14:23:55 +0700

    set up

---
+++ pages/Espisode.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Espisode = () => {
+  return (
+    <View>
+      <Text>Espisode</Text>
+    </View>
+  )
+}
+
+export default Espisode
