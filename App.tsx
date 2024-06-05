a4e27f0efe0f4d705eca5b070a62194a1d20302f
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-06-05 17:08:33 +0700

    update Home view

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { TouchableOpacity, Text } from "react-native";
-
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 import Home from "./pages/Home";
 import Espisode from "./pages/Espisode";
@@ -18,51 +18,52 @@ export default function App() {
     return (
       <Tab.Navigator>
         <Tab.Screen
-          name="Now in Android"
+          name="For you"
           component={Home}
           options={({ navigation }) => ({
             headerTitleAlign: "center",
+            headerTitle: "Now in Android",
             headerLeft: () => (
               // Add the headerLeft function for the left button
               <TouchableOpacity onPress={() => console.log("handle")}>
-                <Text>Back</Text>
+                <Icon name="search" style={styles.headerButton} />
               </TouchableOpacity>
             ),
             headerRight: () => (
               // Add the headerLeft function for the left button
               <TouchableOpacity onPress={() => console.log("handle")}>
-                <Text>Back</Text>
+                <Icon name="user" style={styles.headerButton} />
               </TouchableOpacity>
             ),
-            // tabBarIcon: ({ color, size }) => (
-            //   // <Icon name="home" color={color} size={size} />
-            // ),
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="home" color={color} size={size} />
+            ),
           })}
         />
         <Tab.Screen
           name="Espisode"
           component={Espisode}
           options={({ navigation }) => ({
-            // tabBarIcon: ({ color, size }) => (
-            //   // <Icon name="home" color={color} size={size} />
-            // ),
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="book" color={color} size={size} />
+            ),
           })}
         />
         <Tab.Screen
           name="Saved"
           component={Save}
           options={({ navigation }) => ({
-            // tabBarIcon: ({ color, size }) => (
-            //   // <Icon name="home" color={color} size={size} />
-            // ),
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="save" color={color} size={size} />
+            ),
           })}
         />
         <Tab.Screen
           options={({ navigation }) => ({
             headerShown: false,
-            // tabBarIcon: ({ color, size }) => (
-            //   <Icon name="address-card" color={color} size={size} />
-            // ),
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="heart" color={color} size={size} />
+            ),
           })}
           name="Interest"
           component={Interest}
@@ -87,3 +88,9 @@ export default function App() {
     </NavigationContainer>
   );
 }
+const styles = StyleSheet.create({
+  headerButton: {
+    paddingHorizontal: 16, // Adjust padding as needed
+    fontSize: 20
+  },
+});
--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,12 +1,8 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 
 const Home = () => {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
-}
+  return <View></View>;
+};
 
-export default Home
+export default Home;
