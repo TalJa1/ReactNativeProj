46f2fb460716db52f0adc1311e004a9adbfe621d
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-06-05 16:09:53 +0700

    Update LoginPage.tsx

--- pages/LoginPage.tsx
+++ pages/LoginPage.tsx
@@ -1,12 +1,110 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Alert,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
 const LoginPage = () => {
-  return (
-    <View>
-      <Text>LoginPage</Text>
-    </View>
-  )
-}
+  const navigation = useNavigation<NativeStackNavigationProp<any>>();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-export default LoginPage
+  const loginCheck = {
+    urname: "taitai",
+    pass: "123",
+  };
+
+  const handleLogin = () => {
+    console.log("Login clicked")
+    if (username === loginCheck.urname && password === loginCheck.pass) {
+      navigation.replace("Root");
+      return;
+    }
+    Alert.alert(
+      "Login Failed",
+      "Invalid username or password. Please try again.",
+      [{ text: "OK", onPress: () => console.log("Alert dismissed") }]
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.logo}>TEST</Text>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.inputText}
+          placeholder="Username"
+          placeholderTextColor="#003f5c"
+          value={username}
+          onChangeText={setUsername}
+        />
+      </View>
+      <View style={styles.inputView}>
+        <TextInput
+          secureTextEntry
+          style={styles.inputText}
+          placeholder="Password"
+          placeholderTextColor="#003f5c"
+          value={password}
+          onChangeText={setPassword}
+        />
+      </View>
+      <TouchableOpacity style={styles.loginBtn} onPress={handleLogin}>
+        <Text style={styles.loginText}>LOGIN</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    fontWeight: "bold",
+    fontSize: 50,
+    color: "#fb5b5a",
+    marginBottom: 40,
+  },
+  inputView: {
+    width: "80%",
+    backgroundColor: "white",
+    borderWidth: 1,
+    borderColor: "#000",
+    borderRadius: 25,
+    height: 50,
+    marginBottom: 20,
+    justifyContent: "center",
+    padding: 20,
+  },
+  inputText: {
+    height: 50,
+    color: "black",
+  },
+  loginBtn: {
+    width: "80%",
+    backgroundColor: "#fb5b5a",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    marginBottom: 10,
+  },
+  loginText: {
+    color: "white",
+  },
+  loginErr: {
+    color: "red",
+  },
+});
+
+export default LoginPage;
